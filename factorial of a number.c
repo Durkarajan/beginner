5ba15e6c14ad3c6cfbd327b862a063764aeeee79
Durkarajan <36723396+Durkarajan@users.noreply.github.com>
2018-02-23 12:13:33 +0530

    Create factorial of a number.c

---
+++ factorial of a number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int n,i;
+unsigned long long factorial=1;
+scanf("%d",&n);
+{
+for(i=1;i<=n;i++)
+{
+factorial*=i;            
+}
+printf("%d",factorial);
+}
+return 0;
+}
